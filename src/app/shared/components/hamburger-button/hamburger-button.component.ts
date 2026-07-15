a3e810ee172105b82d850a5d1bc1a941ed8a294f
Sixto Mujica <sixto.mujica@kyndryl.com>
2026-07-15 17:28:53 -0500

    refactor: remove unused custom components

--- src/app/shared/components/hamburger-button/hamburger-button.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hamburger-button',
-  templateUrl: './hamburger-button.component.html',
-  styleUrl: './hamburger-button.component.scss',
-})
-export class HamburgerButtonComponent {}
--- src/app/shared/components/promo-banner/promo-banner.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-promo-banner',
-  imports: [],
-  templateUrl: './promo-banner.component.html',
-  styleUrl: './promo-banner.component.scss',
-})
-export class PromoBannerComponent {}
