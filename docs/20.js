b54bb1e0dd7cfb8897da52190d27b0bcbc31cb3d
Sixto Mujica <sixto.mujica@kyndryl.com>
2026-07-22 12:35:06 -0500

    build: regenerar GitHub Pages con base-href

--- docs/20.js
+++ docs/20.js
@@ -36,140 +36,137 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   routes: () => (/* binding */ routes)
 /* harmony export */ });
+/* harmony import */ var _shell_shell_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./shell/shell.component */ 3100);
+
 const routes = [
-  /*  {
-    path: '',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.landing,
-        exposedModule: './Landing',
-      }).then((m) => m.LandingComponent),
-  }, */
-  /*  {
-    path: 'auth',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.auth,
-        exposedModule: './Auth',
-      }).then((m) => m.AuthComponent),
-  },
-  
-  {
-    path: 'user',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.user,
-        exposedModule: './User',
-      }).then((m) => m.UserComponent),
-  },
-  
-  {
-    path: 'wallet',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.wallet,
-        exposedModule: './Wallet',
-      }).then((m) => m.WalletComponent),
-  },
-  
-  {
-    path: 'recharges',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.recharges,
-        exposedModule: './Recharges',
-      }).then((m) => m.RechargesComponent),
-  },
-  
-  {
-    path: 'notifications',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.notifications,
-        exposedModule: './Notifications',
-      }).then((m) => m.NotificationsComponent),
-  },
-  
-  {
-    path: 'support',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.support,
-        exposedModule: './Support',
-      }).then((m) => m.SupportComponent),
-  },
-  
-  {
-    path: 'tinka',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.tinka,
-        exposedModule: './Tinka',
-      }).then((m) => m.TinkaComponent),
-  },
-  
-  {
-    path: 'kabala',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.kabala,
-        exposedModule: './Kabala',
-      }).then((m) => m.KabalaComponent),
-  },
-  
-  {
-    path: 'ganadiario',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.ganadiario,
-        exposedModule: './Ganadiario',
-      }).then((m) => m.GanadiarioComponent),
-  },
-  
-  {
-    path: 'ganagol',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.ganagol,
-        exposedModule: './Ganagol',
-      }).then((m) => m.GanagolComponent),
-  },
-  
-  {
-    path: 'kinelo',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.kinelo,
-        exposedModule: './Kinelo',
-      }).then((m) => m.KineloComponent),
-  },
-  
-  {
-    path: 'videoloterias',
-    loadComponent: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: environment.remotes.videoloterias,
-        exposedModule: './Videoloterias',
-      }).then((m) => m.VideoloteriasComponent),
-  }, */
-  /*  {
-    path: '**',
-    redirectTo: '',
-  }, */
-];
+/*  {
+  path: '',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.landing,
+      exposedModule: './Landing',
+    }).then((m) => m.LandingComponent),
+}, */
+/*  {
+  path: 'auth',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.auth,
+      exposedModule: './Auth',
+    }).then((m) => m.AuthComponent),
+},
+   {
+  path: 'user',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.user,
+      exposedModule: './User',
+    }).then((m) => m.UserComponent),
+},
+   {
+  path: 'wallet',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.wallet,
+      exposedModule: './Wallet',
+    }).then((m) => m.WalletComponent),
+},
+   {
+  path: 'recharges',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.recharges,
+      exposedModule: './Recharges',
+    }).then((m) => m.RechargesComponent),
+},
+   {
+  path: 'notifications',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.notifications,
+      exposedModule: './Notifications',
+    }).then((m) => m.NotificationsComponent),
+},
+   {
+  path: 'support',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.support,
+      exposedModule: './Support',
+    }).then((m) => m.SupportComponent),
+},
+   {
+  path: 'tinka',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.tinka,
+      exposedModule: './Tinka',
+    }).then((m) => m.TinkaComponent),
+},
+   {
+  path: 'kabala',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.kabala,
+      exposedModule: './Kabala',
+    }).then((m) => m.KabalaComponent),
+},
+   {
+  path: 'ganadiario',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.ganadiario,
+      exposedModule: './Ganadiario',
+    }).then((m) => m.GanadiarioComponent),
+},
+   {
+  path: 'ganagol',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.ganagol,
+      exposedModule: './Ganagol',
+    }).then((m) => m.GanagolComponent),
+},
+   {
+  path: 'kinelo',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.kinelo,
+      exposedModule: './Kinelo',
+    }).then((m) => m.KineloComponent),
+},
+   {
+  path: 'videoloterias',
+  loadComponent: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: environment.remotes.videoloterias,
+      exposedModule: './Videoloterias',
+    }).then((m) => m.VideoloteriasComponent),
+}, */
+/*  {
+  path: '**',
+  redirectTo: '',
+}, */
+{
+  path: '',
+  component: _shell_shell_component__WEBPACK_IMPORTED_MODULE_0__.ShellComponent
+}, {
+  path: '**',
+  redirectTo: ''
+}];
 
 /***/ },
 
