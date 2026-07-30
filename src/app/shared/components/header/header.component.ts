6f1e0c3c22164287fd441b6b84ce0f821d7b89e6
Sixto Mujica <sixto.mujica@kyndryl.com>
2026-07-30 11:08:06 -0500

    update styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,20 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
-import { ButtonComponent } from '../button/button.component';
+import { TkHeaderComponent } from '@kindryl/tinka-ui';
 
-import { MobileSidebarComponent } from '../mobile-sidebar/mobile-sidebar.component';
-import { UserSessionComponent } from '../user-session/user-session.component';
 import { AuthService } from '../../../core/auth/auth.service';
-import { HamburgerButtonComponent } from '../hamburger-button/hamburger-button.component';
+
 @Component({
   selector: 'app-header',
-  imports: [
-    ButtonComponent,
-    HamburgerButtonComponent,
-    MobileSidebarComponent,
-    UserSessionComponent,
-  ],
+  imports: [TkHeaderComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   private readonly authService = inject(AuthService);
@@ -33,9 +25,11 @@ export class HeaderComponent {
   closeMobileMenu(): void {
     this.isMobileMenuOpen.set(false);
   }
+
   startLogin(): void {
     this.authService.startPamLogin();
   }
+
   logout(): void {
     this.authService.logout();
   }
